1656448396deb232ccdadaf20ff65aca1069e5ee
GusJelly <gugatpm@gmail.com>
2024-01-06 12:25:44 +0000

    new fish path stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,10 @@
+# Commands to run in interactive sessions can go here
 if status is-interactive
-    # Commands to run in interactive sessions can go here
 end
+
+# Path fuckery
+fish_add_path "$HOME/.local/bin/"
+fish_add_path "$HOME/.nix-profile/bin/"
+fish_add_path "/nix/var/nix/profiles/default/bin/"
 
 eval "$(starship init fish)"
---
+++ fish/functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/home/gustavo/.fzf/shell/key-bindings.fish
