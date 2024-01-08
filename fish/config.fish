80333fee8298f0c8272438be1f48845a8c66cbe5
GusJelly <gugatpm@gmail.com>
2024-01-08 03:07:37 +0000

    new fish stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,22 @@
-# Commands to run in interactive sessions can go here
-if status is-interactive
-end
+# Vi mode cursor changing
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_visual block
 
 # Path fuckery
 fish_add_path "$HOME/.local/bin/"
 fish_add_path "$HOME/.nix-profile/bin/"
 fish_add_path "/nix/var/nix/profiles/default/bin/"
 
-# Vi mode cursor changing
-set fish_cursor_default block
-set fish_cursor_insert line
-set fish_cursor_replace_one underscore
-set fish_cursor_visual block
+# Aliases
+alias ls="eza"
+alias lsa="eza -a"
+alias l="eza -l"
+alias la="eza -la"
+
+# Commands to run in interactive sessions can go here
+if status is-interactive
+end
 
 eval "$(starship init fish)"
